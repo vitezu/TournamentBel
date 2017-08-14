6e31d245d0bb7c78ccfc0c0c7cab8468bf8a55f5
Pavel_Z <zubrytski.pavel@gmail.com>
2017-08-14 14:39:54 +0300

    add test servlet

---
+++ tournamentbel-web/src/main/java/com/intelisoft/tournamentbel/web/servlets/MatchesServlet.java
@@ -0,0 +1,38 @@
+package com.intelisoft.tournamentbel.web.servlets;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/matches")
+    public class MatchesServlet extends HttpServlet {
+
+        @Override
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            super.doPost(request, response);
+        }
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+
+            out.println("Hello Zubrytski.Pavel");
+//            String varTextA = "Hello World!";
+//            request.setAttribute("textA", varTextA);
+//            String varTextB = "It JSP.";
+//            request.setAttribute("textB", varTextB);
+//
+//            RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+//            dispatcher.forward(request, response);
+        }
+    }
+
