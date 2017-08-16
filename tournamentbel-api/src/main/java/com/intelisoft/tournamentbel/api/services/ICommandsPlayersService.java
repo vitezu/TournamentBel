9a59858b9adc04be21aab3ef07142820b2977807
Zubritsky.Pavel <zubritsky.pavel@gmail.com>
2017-08-16 23:21:09 +0300

    added delete method

--- tournamentbel-api/src/main/java/com/intelisoft/tournamentbel/api/services/ICommandsPlayersService.java
+++ tournamentbel-api/src/main/java/com/intelisoft/tournamentbel/api/services/ICommandsPlayersService.java
@@ -4,4 +4,6 @@ import com.intelisoft.tournamentbel.entity.CommandsPlayers;
 
 public interface ICommandsPlayersService  {
     public void add (CommandsPlayers commandsPlayers);
+    public void delete (CommandsPlayers commandsPlayers);
+
 }
