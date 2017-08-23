729968c76daa7e8fb93fa39180e021f2f942517b
Pavel_Z <zubrytski.pavel@gmail.com>
2017-08-23 14:57:10 +0300

    added some html code

--- tournamentbel-web/src/main/java/com/intelisoft/tournamentbel/web/servlets/MatchesServlet.java
+++ tournamentbel-web/src/main/java/com/intelisoft/tournamentbel/web/servlets/MatchesServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 
 @WebServlet("/MyServlet")
@@ -41,12 +42,31 @@ MatchesServiceImpl matchesService = new MatchesServiceImpl();
 
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
+        //String job = request.getParameter("job");
+        String[] jobs = request.getParameterValues("job");
+        String gender = request.getParameter("gender");
+        if (gender==null){
+            gender = "";
+        }
+        String age18 = request.getParameter("age18");
+        if (age18 == null) {
+            age18 = "НЕТ";
+        }
         System.out.println(firstName);
         System.out.println(lastName);
 
         response.setContentType("text/html; charset = UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Профиль сотрудника</h3>");
+        out.println("Имя: " + firstName + "<br>");
+        out.println("Фамилия: " + lastName + "<br>");
+        //out.println("Профессия: " + job + "<br>");
+        out.println("Профессия: " + Arrays.deepToString(jobs) + "<br>");
+        out.println("Пол: " + gender + "<br>");
+        out.println("Старше 18? " + age18 + "<br>");
+        out.close();
 
-        response.getWriter().print("<h3> Heloo!!ServlPost " + firstName + " " + lastName + "</h3>");
+
     }
     }
 
